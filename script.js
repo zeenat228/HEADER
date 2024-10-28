c83b91ec949bb2cc6d63f5943d8bbcbecf5f274d
zeenat228 <zflsu22@yahoo.com>
2024-10-28 13:43:57 +0600

    responsive

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Toggle navigation menu on small screens
+document.querySelector('.hamburger').addEventListener('click', function () {
+    document.querySelector('.navbar').classList.toggle('active');
+});
